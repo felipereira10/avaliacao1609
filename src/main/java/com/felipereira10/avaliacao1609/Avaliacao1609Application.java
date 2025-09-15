53f94d2c5fea80d08026f652459e8c75e45daf45
Felipe Pereira <126419915+felipereira10@users.noreply.github.com>
2025-09-15 08:43:41 -0300

    feat. adjustments

---
+++ src/main/java/com/felipereira10/avaliacao1609/Avaliacao1609Application.java
@@ -0,0 +1,13 @@
+package com.felipereira10.avaliacao1609;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Avaliacao1609Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Avaliacao1609Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipereira10/avaliacao1609/Avaliacao1609ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipereira10.avaliacao1609;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Avaliacao1609ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
